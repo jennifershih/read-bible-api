5de51a2d41e1b9ca63c1e13b0ba2887da3c9de13
jennifershih <s01059810@gmail.com>
2023-03-10 21:35:30 +0800

    Feat: call bible api

--- src/bible/bible.controller.ts
+++ src/bible/bible.controller.ts
@@ -1,5 +1,12 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { BibleVerseGeneratorService } from 'src/bible/bible.service';
+import axios from 'axios';
 
 @Controller('get-bible-verse-from-mood')
 export class BibleController {
@@ -9,9 +16,37 @@ export class BibleController {
 
   @Post()
   async getBibleVerseFromMood(@Body() body: { mood: string }) {
-    const prompt = `今天我的心情是 ${body.mood},請依照我的心情提供來自現代標點和合本的聖經資訊,格式是書卷＠章＠節給我。例如：約翰福音＠3＠16。\n`;
+    const prompt = `今天我的心情是 ${body.mood},請依照我的心情提供來自現代標點和合本的聖經資訊,格式是書卷＠章＠節給我。例如genesis：＠3＠16。\n`;
     const generatedVerse =
       await this.bibleVerseGeneratorService.generateBibleVerse(prompt);
-    return { verse: generatedVerse };
+
+    const verseMatch = generatedVerse.match(
+      /^(.+?)(?::|：)?[@＠](\d+)[@＠](\d+)(?:-(\d+))?/,
+    );
+
+    if (!verseMatch) {
+      throw new HttpException(
+        'Failed to generate verse.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const bookNames = require('./bookNames');
+    const chineseBookName = verseMatch[1];
+    const englishBookName = bookNames[chineseBookName];
+    const chapter = parseInt(verseMatch[2]);
+    const verseNum = parseInt(verseMatch[3]);
+    const url = `https://bible-api.com/${englishBookName}+${chapter}:${verseNum}`;
+    try {
+      const response = await axios.get(url);
+      const text = response.data.text;
+      return {
+        verse: `${chineseBookName} ${chapter}:${verseNum} ${text}`,
+      };
+    } catch (error) {
+      console.error(error);
+      return { verse: 'Sorry, something went wrong.' };
+    }
   }
 }
