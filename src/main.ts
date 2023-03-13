daf79c6e745047020abeaa3a810ae8c30c5275d5
jennifershih <s01059810@gmail.com>
2023-03-13 14:21:50 +0800

    Feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://jennifershih.github.io'],
+    credentials: true,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(3000);
 }
+
 bootstrap();
